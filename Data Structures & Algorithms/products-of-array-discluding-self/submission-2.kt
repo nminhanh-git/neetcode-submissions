5cac1fdb4849885d4e047c739d47f35aa90d8ece
Adrian Nguyen (Minh Anh) <30652031+nminhanh-git@users.noreply.github.com>
2026-06-15 23:33:18 +0700

    Add: products-of-array-discluding-self - submission-2

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-2.kt
@@ -0,0 +1,18 @@
+class Solution {
+    fun productExceptSelf(nums: IntArray): IntArray {
+        val res = IntArray(nums.size) { 1 }
+
+        var prefix = 1
+        for(i in 0 until nums.size) {
+            res[i] = prefix
+            prefix *= nums[i]
+        }
+
+        var postfix = 1
+        for (i in nums.size - 1 downTo 0) {
+            res[i] *= postfix
+            postfix *= nums[i]
+        }
+        return res
+    }
+}
